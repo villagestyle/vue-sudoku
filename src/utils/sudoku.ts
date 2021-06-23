f26e69d546c9b62128a50991c138d19160229100
villagestyle <1044223679@qq.com>
2021-06-23 17:51:40 +0800

    update 完善数独

--- src/utils/sudoku.ts
+++ src/utils/sudoku.ts
@@ -15,19 +15,11 @@ function takeCell(origin: number[][], i: number) {
   let el = SUDOKUEL.slice();
   let num = 0;
   let success = false;
-  if (i === 0) {
-    for (let j = 0; j < 9; j++) {
-      [num, el] = randomNum(el);
-      arr[j] = num;
-    }
+  for (let j = 0; j < 9; j++) {
+    el = takeSurplusArr(origin, j, i);
+    [num] = randomNum(el);
+    arr[j] = num;
     origin[i] = arr;
-  } else {
-    for (let j = 0; j < 9; j++) {
-      el = takeSurplusArr(origin, j, i);
-      [num] = randomNum(el);
-      arr[j] = num;
-      origin[i] = arr;
-    }
   }
   success = arr.filter(d => d).length === 9;
   return success;
@@ -40,7 +32,7 @@ function randomNum(arr: number[] = SUDOKUEL): [number, number[]] {
   return [num, surplusArr];
 }
 
-function takeSurplusArr(origin: number[][], x: number, y: number) {
+function takeSurplusArr(origin: number[][], x: number, y: number, options = { excludeSelf: false }) {
   const SUDOKUELCopy = SUDOKUEL.slice();
   const originNum = origin[y][x];
   const nineGridElArr = getNinePlaceGrid(origin, x, y).filter(
@@ -48,8 +40,13 @@ function takeSurplusArr(origin: number[][], x: number, y: number) {
   );
   const nineSurplusElArr = SUDOKUELCopy.filter(d => !nineGridElArr.includes(d));
 
-  const row = origin.map(d => d[x]);
-  const col = origin[y];
+  let row = origin.map(d => d[x]);
+  let col = origin[y];
+
+  if (options.excludeSelf) {
+    row = row.filter((d, index) => index !== y);
+    col = col.filter((d, index) => index !== x);
+  }
 
   const rowSurplus = nineSurplusElArr.filter(d => !row.includes(d));
   const colSurplus = rowSurplus.filter(d => !col.includes(d));
@@ -65,23 +62,79 @@ function getNinePlaceGrid(origin: number[][], x: number, y: number): number[] {
   const arr = [];
   // 获取所在九宫格
   for (let i = 0; i < 9; i++) {
-    arr.push(copy[row * 3 + Math.floor(i / 3)][col * 3 + (i % 3)]);
+    const el = copy[row * 3 + Math.floor(i / 3)][col * 3 + (i % 3)];
+    arr.push(el);
   }
 
   return arr;
 }
 
 export function digHole(arr: number[][], num: number) {
-    for (let i = 0; i < num; i++) {
-        const [randomX] = randomNum(SUDOKUEL);
-        const [randomY] = randomNum(SUDOKUEL);
+  for (let i = 0; i < num; i++) {
+    const [randomX] = randomNum(SUDOKUEL);
+    const [randomY] = randomNum(SUDOKUEL);
 
-        if (arr[randomX][randomY]) {
-            arr[randomX][randomY] = 0;
-        } else {
-            i--;
-        }
+    if (arr[randomX - 1][randomY - 1]) {
+      arr[randomX - 1][randomY - 1] = 0;
+    } else {
+      i--;
     }
+  }
 
-    return arr;
+  return formatValue(arr);
+}
+
+function formatValue(arr: number[][]) {
+  const result: SudokuElList[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    const list: SudokuElList = [];
+    for (let j = 0; j < arr[i].length; j++) {
+      const value = arr[i][j];
+      const obj = {
+        value,
+        draft: [],
+        readonly: !!value,
+        error: false
+      };
+      list[j] = obj;
+    }
+    result.push(list);
+  }
+  return result;
+}
+
+export function checkError(data: SudokuElList[]) {
+  const copy = data.slice();
+  const transformArr = [];
+  for (let i = 0; i < copy.length; i++) {
+    const arr = copy[i].map(d => d.value);
+    transformArr.push(arr);
+  }
+
+  const result: [number, number][] = [];
+  for (let i = 0; i < transformArr.length; i++) {
+    for (let j = 0; j < transformArr[i].length; j++) {
+      const el = copy[i][j];
+      if (!el.readonly) {
+        const arr = takeSurplusArr(transformArr, j, i, { excludeSelf: true });
+        console.log(arr);
+        console.log({i, j});
+        if (!arr.includes(transformArr[i][j])) {
+          result.push([i, j]);
+        }
+      }
+    }
+  }
+
+  result.map(d => data[d[0]][d[1]].error = true);
+
+  return data;
+}
+
+export type SudokuElList = SudokuEl[];
+export interface SudokuEl {
+  value: number;
+  draft: number[];
+  readonly: boolean;
+  error: boolean;
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,49 +4,111 @@
       <tr v-for="(col, index) in data.grid" :key="index">
         <td
           class="box"
-          :class="{ active: active === index * 9 + rowIndex }"
+          :class="{
+            active: active === index * 9 + rowIndex,
+            disabled: item.readonly,
+            filled: !item.readonly,
+            error: item.error,
+          }"
           v-for="(item, rowIndex) in col"
-          :key="item"
-          @click="active = index * 9 + rowIndex"
+          :key="rowIndex"
+          @click="active = item.readonly ? -1 : index * 9 + rowIndex"
         >
-          {{ item || '' }}
+          {{ item.value || "" }}
+          <span
+            class="draft"
+            v-for="(draft, index) in item.draft"
+            :key="index"
+            >{{ draft }}</span
+          >
         </td>
       </tr>
     </table>
 
     <section class="keyboard">
-      <div v-for="key in keybord" :key="key" class="box">{{ key }}</div>
+      <div v-for="key in keybord" :key="key" class="box" @click="fill(key)">
+        {{ key }}
+      </div>
+      <div
+        class="box"
+        :class="{ active: mode === 1 }"
+        @click="mode === 1 ? (mode = 0) : (mode = 1)"
+      >
+        草稿
+      </div>
+    </section>
+
+    <section class="check" @click="check">
+      检查
     </section>
   </main>
 </template>
 
 <script lang="ts">
 import { AppStore } from "../../store/modules/app";
-import { drawNinePlaceGrid, digHole } from "../../utils/sudoku";
+import { drawNinePlaceGrid, digHole, SudokuElList, checkError } from "../../utils/sudoku";
 import { defineComponent, onMounted, reactive, ref } from "vue";
 
+type EditMode = 0 | 1;
+
 export default defineComponent({
-  mounted() {
-    console.log(AppStore.getSysNo);
-  },
   setup() {
     const keybord = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     const active = ref<Nullable<number>>(null);
+    const mode = ref<EditMode>(0);
     const data = reactive<{
-      grid: number[][]
+      grid: SudokuElList[];
     }>({
       grid: [[]]
     });
 
     onMounted(() => {
-      const level = prompt('请选择难度(1-9, 默认为1)：', '1');
-      data.grid = digHole(drawNinePlaceGrid(), Number(level) * 15 - 8);
-    })
+      const level = prompt("请选择难度(1-9, 默认为3)：", "3");
+      let levelNum = Number(level) || 3;
+      levelNum = levelNum > 9 ? 9 : levelNum < 1 ? 1 : levelNum;
+      data.grid = digHole(drawNinePlaceGrid(), levelNum * 7);
+    });
+
+    const fill = (key: number) => {
+      if (active.value === null || active.value === -1) {
+        return;
+      }
+
+      const x = active.value % 9;
+      const y = Math.floor(active.value / 9);
+      data.grid[y][x].error = false;
+
+      if (mode.value === 1) {
+        fillDraft(x, y, key);
+        return;
+      }
+
+      data.grid[y][x].value = key;
+      data.grid[y][x].draft = [];
+    };
+
+    const fillDraft = (x: number, y: number, key: number) => {
+      data.grid[y][x].value = 0;
+      const set = new Set(data.grid[y][x].draft);
+      if (set.has(key)) {
+        set.delete(key);
+      } else {
+        set.add(key);
+      }
+      data.grid[y][x].draft = Array.from(set);
+    };
+
+    const check = () => {
+      data.grid = checkError(data.grid);
+    }
 
     return {
       data,
       active,
-      keybord
+      keybord,
+      fill,
+      mode,
+      check
     };
   }
 });
@@ -68,8 +130,22 @@ export default defineComponent({
 }
 
 .box.active {
-  background-color: rgb(65, 192, 255);
+  background-color: rgb(190, 223, 240);
   color: #fff;
+}
+
+.box.filled {
+  color: rgb(0, 155, 233);
+}
+
+.box.error {
+  background-color: rgb(245, 94, 94);
+  color: #fff;
+}
+
+.box.disabled {
+  color: #000;
+  background-color: #fff;
 }
 
 table {
@@ -111,5 +187,22 @@ td {
   margin: 8px;
   cursor: pointer;
   user-select: none;
+}
+
+.draft {
+  display: inline-block;
+  line-height: 20px;
+  font-size: 11px;
+  float: left;
+  width: 33%;
+}
+
+.check {
+  display: inline-block;
+  margin: 20px auto;
+  width: 80px;
+  border: 1px solid rgb(185, 182, 182);
+  cursor: pointer;
+  padding: 15px 30px;
 }
 </style>
